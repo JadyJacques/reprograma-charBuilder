5a35543dc819d4b2f2cba4387d6e71eb41b2493a
Jacques <810617@cognizant.com>
2019-12-10 23:13:54 -0200

    changing how the app starts

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./src/app")
-const port = process.env.new_port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log(`app está rodando na porta ${port}`)
